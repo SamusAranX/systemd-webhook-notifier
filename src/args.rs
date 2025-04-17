0fa8e4e3bc0bc98bb5c3a7c414c534f4c68993f7
Emma Alyx Wunder <hallo@emmalyx.dev>
2025-04-17 20:02:19 +0200

    Made DiscordArgs' remaining options settable via env var

--- src/args.rs
+++ src/args.rs
@@ -3,10 +3,10 @@ use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 pub struct DiscordArgs {
-	#[arg(short, long, help = "PLACEHOLDER")]
+	#[arg(short, long, env = "DISCORD_THUMBNAIL", help = "The thumbnail to display as part of the embed")]
 	pub thumbnail: Option<String>,
 
-	#[arg(short, long, default_value_t = 0x5E5CE6, help = "PLACEHOLDER")]
+	#[arg(short, long, env = "DISCORD_COLOR", default_value_t = 0x5E5CE6, help = "The color to apply to the embed")]
 	pub color: u32,
 
 	#[arg(short, long, env = "DISCORD_WEBHOOK", help = "The URL to send the webhook payload to")]
